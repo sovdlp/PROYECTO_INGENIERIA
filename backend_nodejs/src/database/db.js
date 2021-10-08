3ee266e786e86ca0bfa61ee4e43a4ad141d74372
sovdlp <sovdlp@hotmail.com>
2021-10-08 13:31:28 -0500

    MONGOATLAS_NUBE

--- backend_nodejs/src/database/db.js
+++ backend_nodejs/src/database/db.js
@@ -1,5 +1,6 @@
 module.exports = {
     //db: "mongodb+srv://ciclo3:ciclo3@cluster0.a6x9d.mongodb.net/utp_grupo62?retryWrites=true&w=majority"
     //db: "mongodb://localhost:27017/utp_grupo62"
-    db: "mongodb://localhost:27017/ingenieriasa"
+    db: "mongodb+srv://sovdlp:proy_ing@cluster0.njoci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    //db: "mongodb://localhost:27017/ingenieriasa"
 }
